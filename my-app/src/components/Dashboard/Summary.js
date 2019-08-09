cda5c0cbe9ed5577ef5927fb394706f0a97b37ec
kirankumarsunkati <kiransrit@yahoo.com>
2019-08-09 10:05:12 +0530

    Add files via upload

---
+++ my-app/src/components/Dashboard/Summary.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import sb from './../../Images/SmartBill.jpeg';
+import Navs from './Navs.js';
+import {Link} from 'react-router-dom';
+import Row from './../UtilityComponents/Row.js';
+
+class Summary extends React.Component{
+render(){
+  this.state =['Summary','Users','Agents','Products'];
+
+  return(
+    <div className="dashboardWrapper">
+          <div className="row topWrapper">
+              <div className="col-3">
+              </div>
+              <div className="col-6">
+                  <img src={sb} alt="sb" className="sblogo"/>
+              </div>
+              <div className="col-3">
+              </div>
+          </div>
+          <div className="row dashboardContainer">
+                {this.state.map( (name,key) => {
+                  {{console.log(this.props,this.props.match.path.split('/'));}}
+                  return <Link to={name} key={key}><Navs  value={name}/></Link>
+                })}
+          </div>
+          <Row name={'Collections'} value={'23423'} isHeader={'true'}></Row>
+          <Row name={'Monthly Total'} value={'367000'}></Row>
+          <Row name={'Pending Amount'} value={'23423'}></Row>
+          <Row name={'Subscriptions'} value={'23423'} isHeader={'true'}></Row>
+          <Row name={'Total customer'} value={'23423'}></Row>
+          <Row name={'Total Subsciptions'} value={'23423'}></Row>
+
+
+      </div>
+  );
+
+}
+}
+export default Summary;
